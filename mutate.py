98073b2f8eb84494daba1d418582f2251af85655
yhhshb <yoshi.itsame@gmail.com>
2022-01-04 17:10:52 +0100

    mutate now returns true number of mutations as additional information

--- mutate.py
+++ mutate.py
@@ -14,12 +14,16 @@ class MType(Enum):
     DELETION = 3
     IDENTITY = 4
 
-def mutate(seq: str, mut_rate: float, indel_fraction: float, ext_prob: float) -> str:
+def mutate(seq: str, mut_rate: float, indel_fraction: float, ext_prob: float) -> tuple[str, int, int, int, int]:
     '''Mutate the given sequence.
     '''
     nseq = list()
     indel_type = MType.IDENTITY
     indel_len = 0
+    substitutions = 0
+    insertions = 0
+    deletions = 0
+    total_indel_len = 0
     for base in seq:
         c = fastx.enc_nuc_table[ord(base)]
         #sys.stderr.write("c: {} -> ".format(c))
@@ -29,6 +33,7 @@ def mutate(seq: str, mut_rate: float, indel_fraction: float, ext_prob: float) ->
                 r = random.random()
                 c = (c + int(r * 3.0 + 1)) & 3
                 indel_type = MType.SUBSTITUTION
+                substitutions += 1
             else:#indel
                 #sys.stderr.write("[muttype]: ")
                 indel_len = 1
@@ -37,9 +42,13 @@ def mutate(seq: str, mut_rate: float, indel_fraction: float, ext_prob: float) ->
                 if (random.random() < 0.5):
                     #sys.stderr.write("deletion -> ")
                     indel_type = MType.DELETION
+                    deletions += 1
+                    total_indel_len -= indel_len
                 else:
                     #sys.stderr.write("insertion -> ")
                     indel_type = MType.INSERTION
+                    insertions += 1
+                    total_indel_len += indel_len
         if (indel_type == MType.INSERTION):
             #sys.stderr.write("[apply] insertion l = {}".format(indel_len))
             while(indel_len > 0):
@@ -55,34 +64,10 @@ def mutate(seq: str, mut_rate: float, indel_fraction: float, ext_prob: float) ->
             #sys.stderr.write(" -> [reset]")
             indel_type = MType.IDENTITY
         #sys.stderr.write("\n")
-    return ''.join(nseq)
+    return ''.join(nseq), substitutions, insertions, deletions, total_indel_len
 
 def fixed_main(args):
-    random.seed(args.s)
-    if args.i == None: fd = sys.stdin
-    elif fastx.is_gzipped(args.i): fd = gzip.open(args.i, "rt")
-    else: fd = open(args.i, "r")
-
-    if (args.o == None): fo = sys.stdout
-    else: fo = open(args.o, "w")
-    prev = [None, None]
-    for name, seq, _ in fastx.read(fd):
-        if prev != [None, None]: 
-            sys.stderr.write("{}|end\n".format(prev[0]))
-            sys.stderr.write("{}\n".format(prev[1][-20:]))
-            fastx.fasta_write(fo, prev[0], mutate(prev[1], args.m, args.d, args.x), False)#write buffer
-        prev[0] = name
-        prev[1] = seq
-    if prev != [None, None]: 
-        sys.stderr.write("{}|end\n".format(prev[0]))
-        sys.stderr.write("{}\n".format(prev[1][-20:]))
-        fastx.fasta_write(fo, prev[0], mutate(prev[1], args.m, args.d, args.x), True)
-    fo.flush()
-    fo.close()
-    fd.close()
-
-def random_main(args):
-    random.seed(args.s)
+    random.seed(args.seed)
     if args.i == None: fd = sys.stdin
     elif fastx.is_gzipped(args.i): fd = gzip.open(args.i, "rt")
     else: fd = open(args.i, "r")
@@ -91,34 +76,85 @@ def random_main(args):
     else: fo = open(args.o, "w")
     if (args.log):
         fl = open(args.log, "w")
-        fl.write("mutation rate,InDel fraction,ext. prob.\n")
+        fl.write("{}\n".format(args.sep.join(["MR","IDF","EP","S","I","D","TIDL"])))
     else:
         fl = None
-    mrrange = (min(args.m), max(args.m))
-    idrange = (min(args.d), max(args.d))
-    eprange = (min(args.x), max(args.x))
+    prev = [None, None]
+    for name, seq, _ in fastx.read(fd):
+        if prev != [None, None]: 
+            # sys.stderr.write("{}|end\n".format(prev[0]))
+            # sys.stderr.write("{}\n".format(prev[1][-20:]))
+            mseq, nsub, nins, ndel, tidl = mutate(prev[1], args.m, args.d, args.x)
+            fastx.fasta_write(fo, prev[0], mseq, False)#write buffer
+            if fl: 
+                ll = [args.m, args.d, args.x, nsub, nins, ndel, tidl]
+                row = args.sep.join(map(str, ll))
+                fl.write("{}\n".format(row))
+        prev[0] = name
+        prev[1] = seq
+    if prev != [None, None]: 
+        # sys.stderr.write("{}|end\n".format(prev[0]))
+        # sys.stderr.write("{}\n".format(prev[1][-20:]))
+        mseq, nsub, nins, ndel, tidl = mutate(prev[1], args.m, args.d, args.x)
+        fastx.fasta_write(fo, prev[0], mseq, True)
+        if fl:
+            ll = [args.m, args.d, args.x, nsub, nins, ndel, tidl]
+            row = args.sep.join(map(str, ll))
+            fl.write("{}\n".format(row))
+    if fl:
+        fl.flush()
+        fl.close()
+    fo.flush()
+    fo.close()
+    fd.close()
+
+def random_main(args):
+    def get_params(mrrange: list, idrange: list, eprange: list) -> tuple[float, float, float]:
+        if (mrrange[0] == mrrange[1]): mut_rate = mrrange[0]
+        else: mut_rate = random.uniform(mrrange[0], mrrange[1])
+        if (idrange[0] == idrange[1]): indel_frac = idrange[0]
+        else: indel_frac = random.uniform(idrange[0], idrange[1])
+        if (eprange[0] == eprange[1]): ext_prob = eprange[0]
+        else: ext_prob = random.uniform(eprange[0], eprange[1])
+        return mut_rate, indel_frac, ext_prob
+    random.seed(args.seed)
+    if args.i == None: fd = sys.stdin
+    elif fastx.is_gzipped(args.i): fd = gzip.open(args.i, "rt")
+    else: fd = open(args.i, "r")
+
+    if (args.o == None): fo = sys.stdout
+    else: fo = open(args.o, "w")
+    if (args.log):
+        fl = open(args.log, "w")
+        fl.write("{}\n".format(args.sep.join(["MR","IDF","EP","S","I","D","TIDL"])))
+    else:
+        fl = None
+    mr = (min(args.m), max(args.m))
+    idr = (min(args.d), max(args.d))
+    epr = (min(args.x), max(args.x))
     prev = [None, None]
     for id, seq, _ in fastx.read(fd):
-        if (mrrange[0] == mrrange[1]): mutation_rate = mrrange[0]
-        else: mutation_rate = random.uniform(mrrange[0], mrrange[1])
-        if (idrange[0] == idrange[1]): indel_fraction = idrange[0]
-        else: indel_fraction = random.uniform(idrange[0], idrange[1])
-        if (eprange[0] == eprange[1]): extension_prob = eprange[0]
-        else: extension_prob = random.uniform(eprange[0], eprange[1])
+        mutation_rate, indel_fraction, extension_prob = get_params(mr, idr, epr)
         if prev != [None, None]: 
-            fastx.fasta_write(mutate(fo, prev[0], prev[1], mutation_rate, indel_fraction, extension_prob))
-            if (fl): fl.write(str(mutation_rate)+'\t'+str(indel_fraction)+'\t'+str(eprange)+'\n')
+            mseq, nsub, nins, ndel, tidl = mutate(prev[1], mutation_rate, indel_fraction, extension_prob)
+            fastx.fasta_write(fo, prev[0], mseq, False)
+            if fl: 
+                ll = [mutation_rate, indel_fraction, extension_prob, nsub, nins, ndel, tidl]
+                row = args.sep.join(map(str, ll))
+                fl.write("{}\n".format(row))
         prev[0] = id
         prev[1] = seq
     if prev != [None, None]:
-        if (mrrange[0] == mrrange[1]): mutation_rate = mrrange[0]
-        else: mutation_rate = random.uniform(mrrange[0], mrrange[1])
-        if (idrange[0] == idrange[1]): indel_fraction = idrange[0]
-        else: indel_fraction = random.uniform(idrange[0], idrange[1])
-        if (eprange[0] == eprange[1]): extension_prob = eprange[0]
-        else: extension_prob = random.uniform(eprange[0], eprange[1])
-        fastx.fasta_write(mutate(fo, prev[0], prev[1], mutation_rate, indel_fraction, extension_prob))
-        if (fl): fl.write(str(mutation_rate)+'\t'+str(indel_fraction)+'\t'+str(eprange)+'\n')
+        mutation_rate, indel_fraction, extension_prob = get_params(mr, idr, epr)
+        mseq, nsub, nins, ndel, tidl = mutate(prev[1], mutation_rate, indel_fraction, extension_prob)
+        fastx.fasta_write(fo, prev[0], mseq, True)
+        if fl:
+            ll = [mutation_rate, indel_fraction, extension_prob, nsub, nins, ndel, tidl]
+            row = args.sep.join(map(str, ll))
+            fl.write("{}\n".format(row))
+    if fl:
+        fl.flush()
+        fl.close()
     fd.close()
     fo.flush()
     fo.close()
@@ -141,7 +177,9 @@ def setup_parser():
     parser_fixed.add_argument("-m", help="mutation probability (substitutions + indels) [0.05]", type=float, default=0.05)
     parser_fixed.add_argument("-d", help="indel fraction of the mutations [0.15]", type=float, default=0.15)
     parser_fixed.add_argument("-x", help="probability that an indel is extended [0.3]", type=float, default=0.3)
-    parser_fixed.add_argument("-s", help="random seed [42]", type=int, default=42)
+    parser_fixed.add_argument("-s", "--seed", help="random seed [42]", type=int, default=42)
+    parser_fixed.add_argument("-l", "--log", help="log file in csv format containing the parameters for each sequence", required=False)
+    parser_fixed.add_argument("-p", "--sep", help="separator used for the log file", type=str, default=',', choices=[',', '\t'])
 
     parser_random = subparsers.add_parser("random", help="mutate the sequences in the FASTX file with random mutation rates")
     parser_random.add_argument("-i", help="input FASTX file to modify [stdin as fasta]", nargs='?')
@@ -149,8 +187,9 @@ def setup_parser():
     parser_random.add_argument("-m", help="range of mutation probabilities [[0.0, 0.7]]", nargs='+', type=float, default=[0.0, 0.7])
     parser_random.add_argument("-d", help="range of indel fractions of the mutations [[0.15]]", nargs='+', type=float, default=[0.15])
     parser_random.add_argument("-x", help="range of probabilities that an indel is extended [[0.3]]", nargs='+', type=float, default=[0.3])
-    parser_random.add_argument("-s", help="random seed [42]", type=int, default=42)
-    parser_random.add_argument("-l", "--log", help="log file in tsv format containing the parameters for each sequence", required=False)
+    parser_random.add_argument("-s", "--seed", help="random seed [42]", type=int, default=42)
+    parser_random.add_argument("-l", "--log", help="log file in csv format containing the parameters for each sequence", required=False)
+    parser_random.add_argument("-p", "--sep", help="separator used for the log file", type=str, default=',', choices=[',', '\t'])
 
     return parser
 
